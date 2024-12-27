4fef3f29c8cd6d4585a295a39c1306a1e384a9ab
mariam Hussien <mariam.hussien.ali@gmail.com>
2024-12-27 20:25:39 +0300

    second module Calculator

---
+++ Module2/Module Assesments/Calculator/main.py
@@ -0,0 +1,14 @@
+def main():   
+    #Calculate age
+    first_number = input("Enter first_number: ")
+    second_number = input("Enter second_number: ")
+    sum = int(first_number) + int(second_number)
+    subtraction = int(first_number) - int(second_number)
+    multiplication = int(first_number) * int(second_number)
+    division = int(first_number) / int(second_number)
+    print("sum = ", sum)   
+    print("subtraction = ", subtraction)   
+    print("multiplication = ", multiplication)   
+    print("division = ", division)     
+
+main() 
