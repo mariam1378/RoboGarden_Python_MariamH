74220a437e3337083f8dc5154132482464fa678e
mariam Hussien <mariam.hussien.ali@gmail.com>
2024-12-27 20:08:20 +0300

    second module Calculate Age

---
+++ Module2/Module Assesments/Calculate Age/main.py
@@ -0,0 +1,8 @@
+def main():   
+    #Calculate age
+    start_year = input("Enter your birth year: ")
+    end_year = input("Enter current year: ")
+    age = int(end_year) - int(start_year)
+    print(age)     
+
+main() 
