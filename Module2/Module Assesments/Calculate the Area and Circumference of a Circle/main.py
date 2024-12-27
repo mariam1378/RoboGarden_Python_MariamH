2d22113f4b419721aa9040479d80a37e2f81a826
mariam Hussien <mariam.hussien.ali@gmail.com>
2024-12-27 20:57:05 +0300

    second module calculate Area and Circumference

---
+++ Module2/Module Assesments/Calculate the Area and Circumference of a Circle/main.py
@@ -0,0 +1,9 @@
+def main():   
+    #Calculate age
+    radius = int(input("Enter radius: "))
+    circumference = 2 * 3.14 * radius
+    area = 3.14 * pow(radius,2)
+    print("circumference = ", circumference) 
+    print("area = ", area)      
+
+main() 
