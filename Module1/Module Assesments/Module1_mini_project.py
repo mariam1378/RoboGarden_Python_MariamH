09fea46aaa24e99f73304f66bdfcd6e89268878a
mariam Hussien <mariam.hussien.ali@gmail.com>
2024-12-27 19:40:00 +0300

    first

---
+++ Module1/Module Assesments/Module1_mini_project.py
@@ -0,0 +1,12 @@
+def main():   
+
+  
+
+    ## Type your code here  
+
+    info = "My name is Mariam Hussien. I'm 25 years old, I'm learning python to be an automation engineer"
+    print(info)
+
+    ##     
+
+main() 
---
+++ Module1/Module Assesments/first.py
@@ -0,0 +1,12 @@
+def main():   
+
+  
+
+    ## Type your code here  
+
+    x = "Welcome to the RoboGarden course."
+    print(x)
+
+    ## 
+
+main() 
