ce41b63d7d4fb7fb76f4c8785cda1d5ca86fc193
mariam Hussien <mariam.hussien.ali@gmail.com>
2024-12-29 11:52:31 +0300

    module 5 fibonacci recursion

--- Module5/Module Assesments/The Fibonacci Function/main.py
+++ Module5/Module Assesments/The Fibonacci Function/main.py
@@ -1,15 +1,24 @@
-def fibonacci(limit):
-    ls = [0,1]
-    ## Type your code here 
-    for i in range(0,limit):
-        fibonacci = ls[i]+ls[i+1]
-        ls.append(fibonacci)
-    print(ls) 
-    return fibonacci
+# def fibonacci(limit):
+#     ls = [0,1]
+#     ## Type your code here 
+#     for i in range(0,limit):
+#         fibonacci = ls[i]+ls[i+1]
+#         ls.append(fibonacci)
+#     print(ls) 
+#     return fibonacci
+
+def rec_fibonacci(limit):
+    if limit == 0:
+        return 0
+    elif limit == 1:  
+        return 1
+    
+    return rec_fibonacci(limit - 1) + rec_fibonacci(limit - 2) 
 
 def main():  
     iterations = int(input("Enter count: "))
-    fib = fibonacci(iterations)
-    return fib
+    fib_num = rec_fibonacci(iterations)
+    print("Fibonacci number is: ",fib_num)
+    return fib_num
 
 main() 
