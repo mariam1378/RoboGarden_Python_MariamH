5a1b696736722b5a9875f4ee870d141820376099
mariam Hussien <mariam.hussien.ali@gmail.com>
2024-12-30 22:46:22 +0300

    edits

--- Module6/Module Assesments/Student Grade Tracker/main.py
+++ Module6/Module Assesments/Student Grade Tracker/main.py
@@ -3,16 +3,14 @@ def average_grade(ls):
     average = total / len(ls)
     return average
 
-def store_in_file(file_name,name,ls):
-    # print("hello essam")
+def store_in_file(file_name,name,ls, average):
     try:
         with open(file_name, "a") as file:
             file.write(name+" ")
             for i in range(len(ls)):
                 strls=str(ls[i])
-                print(type(strls))
                 file.write(strls+" ")
-            file.write("\n")
+            file.write(str(average) +"\n")
             print("Your entry has been saved!")
     except PermissionError:
         print("Error: You do not have permission to write to this file.")
@@ -29,30 +27,20 @@ def grade_tracker(file_name):
         grade = int(input(f"enter {subjects[i]} grade : "))
         grades_list.append(grade)
     
-    store_in_file(file_name,student_name,grades_list)
     average = average_grade(grades_list)
+    store_in_file(file_name,student_name,grades_list, average)
     return grades_list, student_name , average
 
 def main():
     file_name = "student_grades.txt"
     while True:
         print("\n--- Grade Tracker ---")
-        print("1. Write a new entry")
-        print("2. View previous entries")
-        print("3. Exit")
         try:
-            choice = int(input("Choose an option: "))
-            if choice == 1:
-                grade_tracker(file_name)
-            else:
-                print("Invalid choice. Please enter 1, 2, or 3.")
+            grade_tracker(file_name)
         except ValueError:
             print("Invalid input. Please enter a numeric value.")
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
-        # file_name = "Grade_Tracker.txt"
-        # grades = grade_tracker()
-        # print(grades)
 
 
 main()
